8ae4faf440b1f89df489bec65171c05029708db2
Max <maxim.bigin@yandex.ru>
2021-03-04 19:48:24 +0300

    create main files

---
+++ kb.py
@@ -0,0 +1,32 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, \
+					InlineKeyboardMarkup, InlineKeyboardButton
+
+# текст кнопок
+button_main = KeyboardButton('🏠 Главное меню')
+button_load = KeyboardButton('💾 Скачать видео')
+button_r = KeyboardButton('🔄 Скачать ещё раз')
+button_lk = KeyboardButton('📰 Личный кабинет')
+button_p = KeyboardButton('💎 Мои кристаллы')
+button_cl = KeyboardButton('📚 Наши каналы')
+button_help = KeyboardButton('💬 Служба поддержки')
+button_back = KeyboardButton('⬅️ Назад')
+button_app = KeyboardButton('📱 Приложение')
+button_site = KeyboardButton('🖥 Cайт')
+button_msite = KeyboardButton('💻 Мобильная версия сайта')
+
+# включение в работу кнопок
+reply_main = ReplyKeyboardMarkup(resize_keyboard=True).add(button_main)
+
+reply_r_main = ReplyKeyboardMarkup(resize_keyboard=True) \
+			.add(button_r).add(button_main)
+
+reply_lk = ReplyKeyboardMarkup(resize_keyboard=True) \
+			.add(button_p).add(button_cl).add(button_main)
+
+reply_load_lk = ReplyKeyboardMarkup(resize_keyboard=True) \
+			.add(button_load).add(button_lk).add(button_help)
+
+reply_back = ReplyKeyboardMarkup(resize_keyboard=True).add(button_back)
+
+reply_device_main = ReplyKeyboardMarkup(resize_keyboard=True) \
+			.add(button_app, button_site).add(button_msite).add(button_main)
