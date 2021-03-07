e85ac2162fa3f07782738ce7f35f75eb5089efec
Max <maxim.bigin@yandex.ru>
2021-03-07 18:53:42 +0300

    temporary delete mailing

--- admin.py
+++ admin.py
@@ -3,26 +3,11 @@ import kb
 from states import Mailing
 from aiogram import types
 from bot import admin_id
-from loader import dp, bot, storage
+from loader import dp, bot
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import CommandStart
 
 # меню для админов
 @dp.message_handler(user_id=admin_id, commands=['admin'])
 async def admin(message: types.Message):
-    await message.answer('👋 Приветствую, Admin – {0.first_name}!\n'
-        'На кнопках отображены твои возможности'.format(message.from_user),
-        reply_markup=kb.reply_menu_admin)
-
-# Рассылка по юзерам
-@dp.message_handler(user_id=admin_id, commands=['mailing'])
-async def mailing(message: types.Message):
-    await message.answer('Пришлите текст рассылки')
-    await Mailing.text.set()
-
-@dp.callback_query_handler(user_id=admin_id, state=Mailing.text)
-async def mailing_start(message: types.Message, state: FSMContext):
-    text = message.text
-    async with state.proxy() as data:
-        data['mail'] = text
-        mail = data['mail']
+    await message.answer('👋 Приветствую, Admin – {0.first_name}!\n'.format(message.from_user))
