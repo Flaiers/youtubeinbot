f36447828b40981eef888922435db26237993b47
Max <BiginMS@stud.kai.ru>
2021-05-27 20:35:39 +0300

    update

--- create.py
+++ create.py
@@ -3,15 +3,17 @@ from bot import key, uid
 import requests
 
 def video_title(link):
-	response = requests.get(link)
+	response = requests.get(f'https://www.youtube.com/watch?v={link}')
 	soup = BeautifulSoup(response.text, 'lxml')
 	title = soup.find("title").text.split(' - ')[0]
+	print(title)
 	return title
 
 def shorten(link):
 	url = f'https://bc.vc/api.php?key={key}&uid={uid}&url={link}&format=text' 
 	response = requests.get(url)
 	create = response.text
+	print(create)
 	return create
 
 def video_link(type, url):
@@ -19,12 +21,14 @@ def video_link(type, url):
 		name = video_title(url)
 		link = f'https://presaver.com/{url}/download/22?title={name}'
 		short = shorten(link)
+		print(link)
 		return short
 
 	elif type == '360':
 		name = video_title(url)
 		link = f'https://presaver.com/{url}/download/18?title={name}'
 		short = shorten(link)
+		print(link)
 		return short
 	
 	elif type == 'img':
