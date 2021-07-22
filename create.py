815d3bdae23ca17daa8eeee8bb5b96c4557e3ebf
Max <BiginMS@stud.kai.ru>
2021-07-22 19:20:22 +0300

    logging

--- create.py
+++ create.py
@@ -1,19 +1,21 @@
 from bot import key, uid
 from bs4 import BeautifulSoup as bs
 
-import requests, urllib.request as req
+import requests, logging, urllib.request as req
 
 
 def video_title(link):
 	page_video = requests.get(f'https://www.youtube.com/watch?v={link}')
 	html_video = bs(page_video.content, 'html.parser')
 	title = html_video.find('title').text
+	logging.info(f'Название видео "{link}" — "{title}"\n\n')
 	return req.pathname2url(title)
 
 def shorten(link):
 	url = f'https://bc.vc/api.php?key={key}&uid={uid}&url={link}&format=text' 
 	response = requests.get(url)
 	create = response.text
+	logging.info(f'Ссылка — "{link}" сокращена\n\n')
 	return create
 
 def video_link(type, url):
