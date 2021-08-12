5cd83e9312132429f1757358ccc7ac891fb91beb
Maxim Bigin <63356200+Flaiers@users.noreply.github.com>
2021-08-13 00:08:07 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,26 @@
+# токен бота 
+TOKEN = '' 
+ 
+# key bc.vc 
+key = '' 
+ 
+# uid bc.vc 
+uid = _ 
+ 
+# admin -@Flaiers 
+admin_id = _ 
+ 
+# telegram api id 
+api_id = _ 
+ 
+# telegram api hash 
+api_hash = '' 
+ 
+# telegram name session 
+name = 'root' 
+ 
+# фразы 
+unknown = ['Я тебя не понимаю 😐', 'Мне непонятно 🙃', 
+	'Твоё сообщение мне непонятно 😕', 'Я не могу понять 🙃', 
+	'Пожалуйста, перефразируйте вопрос 😉', 'Не могу разобрать 😕', 
+	'Некорректный вопрос 🧐', 'Такое я ещё не понимаю 🙁']
